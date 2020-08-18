647730863bf7eba93aa00af29ad4f9b30ec2be38
Awunjia Serge Atabong <awunjiaserge@yahoo.com>
2020-08-18 13:12:16 +0100

    Create MainFrame.java
    This is the frame that opens when the right password is entered, feel free to load your any program here

---
+++ MainFrame.java
@@ -0,0 +1,18 @@
+package com.ASAtech;
+
+import javax.swing.*;
+import java.io.File;
+
+public class MainFrame extends JFrame {
+
+    private File file;
+    private String fileName, fileType;
+
+    public MainFrame(){
+        super("Main Frame");
+        
+        //Write your program here, that runs after the user has enetered the right password 
+    }
+    
+    //Open source is love
+}
